fecd70ce7c43dcc3069faa3e5fdd941609469c96
Jeff Becker <jeff@i2p.rocks>
2021-06-22 07:34:53 -0400

    add script for rebuilding docker image

---
+++ contrib/build-docker-images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# the registry server to use
+registry=$1
+
+test "x$registry" != "x" || exit 1
+
+for file in ${@:2} ; do
+    name="$(echo $file | cut -d'.' -f1)"
+    echo "rebuild $name"
+    docker build -f $file -t $registry/$name --pull --no-cache .
+    docker push $registry/$name
+done
